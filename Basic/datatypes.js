017bd94b0164e830b9c1af88ce0b0ef607298668
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-07-22 17:33:29 +0000

    learned about data types

---
+++ Basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version 
+
+
+// alert("hello") we are using node js, not browser
+
+// different types of data types 
+
+// 1.number => 2 to power 53
+// 2.string => ""
+// 3.bigint
+// 4.boolean
+// 5.null
+// 6.undefined
+// 7.symbol
+
+
+
+// Object
+
+console.log(typeof null);
