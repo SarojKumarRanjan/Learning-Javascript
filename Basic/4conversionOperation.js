641b2a96981f5bc7937b017a25d4b14f3afd0c68
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-05 06:01:58 +0000

    conversion of data types

---
+++ Basic/4conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+console.log( typeof valueInNumber);
+
+
+//"33" => 33
+
+//"33abc" => NaN
+
+//true => 1
+
+//false => 0 
+
+let one=1
+
+let booleanOne= Boolean(one)
+
+console.log(booleanOne);
+
+
+/* 1 => true
+0 => false
+"" => false
+"saroj" => true */
