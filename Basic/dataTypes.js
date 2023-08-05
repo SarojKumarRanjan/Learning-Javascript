b132a9cd6715440023fb7ba71671f73f906aec9d
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-05 10:04:16 +0000

    About data types

---
+++ Basic/dataTypes.js
@@ -0,0 +1,20 @@
+// primitive data types
+// #7 types
+
+/* 1=> String
+2=> Number
+3=> Boolean
+4=> null
+5=> undefined
+6=> Symbol : const id = Symbol("123")
+
+7=> BigInt */
+
+// non-primitive or reference data types
+
+/* 1=>Arrays
+2=>Objects
+3=>Functions */
+
+// javascript is a dynamic type language in which we do not need to specifiy the data types it automatically detects the type from the value assigned to it.
+
