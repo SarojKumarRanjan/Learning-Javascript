c67dfcd83bda8b13d8ede08a585dc61749c83a23
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-09 18:05:36 +0000

    All about conditionals statement

---
+++ Control-flow/1conditional.js
@@ -0,0 +1,100 @@
+//   IF statment
+
+/* if(condition){
+
+    statement
+}
+ */
+// == => this check the equalatity only if the given two value is true or not 
+
+// === => this check the equality as well as the type of the two value of which we are checking.
+
+if(2=="2"){
+   // console.log("excuted");
+}
+
+// "&&" => is used to check the multiple condition 
+
+// "||" => is used for or condition.
+
+// ************** SWITCH CASE *************
+
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
+/* const month = Math.floor((Math.random()*10)+1)
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("february");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    case 5:
+        console.log("may");
+        
+        break;
+    case 6:
+        console.log("june");
+        
+        break;
+
+    default:console.log("invalid input");
+        break;
+} */
+
+/* const userEmail = {}  // which values are truth and which are false
+
+
+
+if(userEmail){
+    console.log("got");
+}
+else{
+    console.log("didn't get the value");
+}
+
+ */
+// Falsy values => false , 0 , -0 , Bigint 0n , "" , null , undfined , NaN
+
+// truthy values => "0" , [] , "false" , " " , {} , function(){} , 
+
+
+
+// Nullish coalescing Operator (??): null, undefined 
+
+
+/* let val1;
+
+// val1 = 5 ?? 10;
+
+val1 = null ?? 10;
+
+
+console.log(val1); */
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+/* const tea = 100
+
+tea > 50 ? console.log("less than 200") : console.log("outside the condition"); */
