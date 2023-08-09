89f5105dabbbdc0af16f1fcb0eb40a61757ccb4a
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-09 18:54:04 +0000

    About for loop

---
+++ Loop/one.js
@@ -0,0 +1,26 @@
+// for 
+
+let array = [1,2,5,8,9,3,7,6]
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+} */
+
+// for each method
+
+
+/* array.forEach(element => {
+
+    console.log(element);
+    
+}); */
+
+
+
+
+
+
+
+
+
+
