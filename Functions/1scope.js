53fffb19d6f0da4907ab0385d288458b8ae1281d
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-09 06:27:10 +0000

    All about scope

---
+++ Functions/1scope.js
@@ -0,0 +1,21 @@
+
+
+if (true) {
+    let a = 10;
+
+    const b = 20;
+    
+    var c = 30; 
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
+// let => it is a local variable 
+
+// const => it is a constant value holder in local variable.
+
+// var => it is a global variable.
