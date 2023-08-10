66f75ea2d05f2958a56f522e033bd65515dd5e48
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-10 09:26:36 +0000

    About while loop

---
+++ Loop/two.js
@@ -0,0 +1,25 @@
+
+
+/* while (2==="2") {
+    console.log("true");
+    
+}
+ */
+let i=1;
+
+/* while (i<=20) {
+     console.log(`The index number is ${i}`);
+
+     i = i+3;
+    
+}
+console.log(i); */ // The scope of the variable is global here.
+
+
+/* do {
+
+    i++;
+    console.log(`The index of the current loop is ${i}`);
+    
+} while (i<=15); */
+
