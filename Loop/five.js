89f63f56b580509733040efdd7222a2c4293bdaa
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-12 19:15:17 +0000

    About filter

--- Loop/five.js
+++ Loop/five.js
@@ -15,6 +15,12 @@ console.log(values);// This gives undefined value. */
 
 const myNum = [1,2,3,4,5,6,7,8,9]
 
- const newNum = myNum.filter( (num) => num > 5)
+ const newNum = myNum.filter( (num) => {
+    return num>4
+ })
 
  console.log(newNum); // tHIS RETURN the value with the applied condition.
+
+
+ 
+
