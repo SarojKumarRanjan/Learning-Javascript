f9a2635a6c8f844c8f1d09448534e8ae03394d80
Saroj Kumar Ranjan <115515109+SarojKumarRanjan@users.noreply.github.com>
2023-08-12 19:00:15 +0000

    About filter,map and reduce

---
+++ Loop/five.js
@@ -0,0 +1,20 @@
+/* const coding = ["java", "c", "c++", "python","javascript"]
+
+const values = coding.forEach ((item) => {
+
+ console.log(item);
+
+ return item;
+
+})
+
+console.log(values);// This gives undefined value. */
+
+
+//***********Usecase of filter********/
+
+const myNum = [1,2,3,4,5,6,7,8,9]
+
+ const newNum = myNum.filter( (num) => num > 5)
+
+ console.log(newNum); // tHIS RETURN the value with the applied condition.
